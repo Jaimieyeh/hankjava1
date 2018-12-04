ea7d5d571894aad6b7ec01d8c3f5640d699e555b
user <user@es302n27>
2018-12-04 12:02:27 +0800

    variables

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -4,7 +4,23 @@ import java.util.Random;
 
 public class PokerGame {	
 	public static void main(String[] args) {
-		int[] flowers = {0x2663, 0x2666, 0x2665, 0x2660};
+		
+		int[] n = new int[10];
+		Card[] c = new Card[10];
+		System.out.println(n[0]);
+		c[0] = new Card(7);
+		System.out.println(c[0].get());
+		
+		
+		Poker poker = new Poker();
+		poker.shuffle();
+		poker.print();
+		
+	
+		
+		
+		
+		/*int[] flowers = {0x2663, 0x2666, 0x2665, 0x2660};
 		int[] nums = new int[5];
 		nums[0] = 77;
 		nums[4] = 99;
@@ -14,10 +30,12 @@ public class PokerGame {
 			System.out.println(nums[i]);
 		}
 		char f = 0x2666;
-		System.out.println(f);
+		System.out.println(f);*/
 		
 	
-		Random random = new Random();
+		
+		
+		/*Random random = new Random();
 		int number = random.nextInt(13)+1;
 		int flower = random.nextInt(4);
 		System.out.print(number);
@@ -35,11 +53,9 @@ public class PokerGame {
 		case 3 :
 			c = 'S';
 			break;
-			
 		}
 		System.out.println(c);
-		
-		
+*/		
 		
 	}
 
